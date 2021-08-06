c0ec25f60afac8ba54ae596d24506260cd32e2b2
Ryan Guthrie <ryanguthrie@zoho.com>
2021-08-06 13:03:26 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const employee = {
+    name:'Ryan',
+    streetAddress: '2020 Kimbrook'
+};
+
+function updateEmployeeWithKeyAndValue() {
+    const updateEmployee = { ...employee };
+
+updateEmployee.name = 'Sam';
+updateEmployee.streetAddress = '11 Broadway';
+return updateEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.name = 'Sam';
+    employee.streetAddress = '12 Broadway';
+    return employee;
+}
+
+function deleteFromEmployeeByKey() {
+    const newEmployee = {...employee}
+    delete newEmployee.name
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    const newEmployee = employee;
+    delete newEmployee.name;
+    return newEmployee;
+}
